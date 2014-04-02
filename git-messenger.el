79a654287a3b5daf564b3dfd5104a684b0adb9c5
Syohei YOSHIDA <syohex@gmail.com>
2014-04-02 14:22:44 +0900

    Bump up version and update Changes

--- git-messenger.el
+++ git-messenger.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-git-messenger
-;; Version: 0.09
+;; Version: 0.10
 ;; Package-Requires: ((popup "0.5.0"))
 
 ;; This program is free software; you can redistribute it and/or modify

533e67f78efdeeb18f0e0a5adcee9b9d41aab490
Syohei YOSHIDA <syohex@gmail.com>
2014-07-06 22:46:16 +0900

    Bump up version and update Changes

--- Cask
+++ Cask
@@ -1,6 +1,6 @@
 (source gnu)
 (source melpa)
 
-(package "git-messenger" "0.12" "Pop-up last commit information of current line")
+(package "git-messenger" "0.13" "Pop-up last commit information of current line")
 
 (depends-on "popup")
--- git-messenger.el
+++ git-messenger.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-git-messenger
-;; Version: 0.12
+;; Version: 0.13
 ;; Package-Requires: ((popup "0.5.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
